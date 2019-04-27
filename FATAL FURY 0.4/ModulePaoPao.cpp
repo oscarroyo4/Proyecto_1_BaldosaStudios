ed4257cb43da686771b7e2abe31b81b91b06baf2
pauraurell <47743764+pauraurell@users.noreply.github.com>
2019-04-27 15:46:37 +0200

    Revert "Added the special atack and small fixes"
    This reverts commit ca533d21fa994f3c546eb50986c8244480ca85b8, reversing
    changes made to d50c768c9947a2677320eb1e4b18821167554a4e.

---
+++ FATAL FURY 0.4/ModulePaoPao.cpp
@@ -0,0 +1,76 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModulePaoPao.h"
+#include "ModulePlayer.h"
+#include "ModuleInput.h"
+#include "ModuleIntro.h"
+#include "ModuleCollision.h"
+#include "ModuleSounds.h"
+#include "ModuleEnemy.h"
+#include "ModuleHUD.h"
+#include "ModuleFadeToBlack.h"
+#include "SDL_image/include/SDL_image.h"
+
+// Reference at https://www.youtube.com/watch?v=OEhmUuehGOA
+
+ModuleScenePaoPao::ModuleScenePaoPao()
+{
+	paopao.PushBack({ 0, 0, 619, 224 });
+	paopao.PushBack({ 0, 224, 619, 224 });
+	paopao.speed = 0.08f;
+}
+
+ModuleScenePaoPao::~ModuleScenePaoPao()
+{}
+
+// Load assets
+bool ModuleScenePaoPao::Start()
+{
+	App->hud->Enable();
+	LOG("Loading background assets");
+	bool ret = true;
+	graphics = App->textures->Load("Assets/Sprites/Pao Pao Cafe Tileset/Pao Pao Cafe Stage.png");
+	music = App->sounds->Load("Assets/Audio/PaoPao.ogg");
+	App->player->Enable();
+	App->enemy->Enable();
+	App->render->camera.x = -530;
+	//Play the music
+	if (Mix_PlayChannel(-1, music, 0) == -1)
+	{
+		LOG("Could not play music. Mix_PlayChannel: %s", Mix_GetError());
+		ret = false;
+	}
+	return ret;
+}
+
+// UnLoad assets
+bool ModuleScenePaoPao::CleanUp()
+{
+	LOG("Unloading ken scene");
+
+	App->paopao->Disable();
+	SDL_DestroyTexture(graphics);
+	App->sounds->Unload(music);
+	App->player->Disable();
+	App->enemy->Disable();
+	App->hud->Disable();
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleScenePaoPao::Update()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(graphics, 0, 0, &(paopao.GetCurrentFrame()), 0.75f);
+
+	if (App->input->keyboard[SDL_SCANCODE_SPACE] == 1)
+	{
+
+		App->fade->FadeToBlack(this, App->scene_intro, 2.5);
+	}
+
+	return UPDATE_CONTINUE;
+}
